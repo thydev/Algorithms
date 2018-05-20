354a4a323fc627480ca7060f8f9bdf53df3eb169
Chanthy Ngin <nginchanthy@gmail.com>
2018-05-20 08:50:45 -0700

    updated sorts

---
+++ Chapter12-Sorts/quicksort.js
@@ -0,0 +1,11 @@
+function partition(arr, start, end) {
+    return pivot index
+}
+
+do quicksort(arr, start = 0, end = arr.length-1)
+
+if (start == end) 
+    return arr
+const pi = partition(arr, start, end )
+quicksort(arr, 0, pi -1)
+quicksort(arr, pi + 1, arr.length-1)
