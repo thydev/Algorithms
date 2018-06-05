1ac70dae5dc34c14fb272c3e94ea05f453782cb5
Chanthy Ngin <nginchanthy@gmail.com>
2018-06-05 16:21:14 -0700

    optimize Bubble sort

--- Chapter12-Sorts/bublesort.js
+++ Chapter12-Sorts/bublesort.js
@@ -1,17 +1,19 @@
 function bublesort(arr) {
     let isSorted = false;
-    while(!isSorted) {
+    while (!isSorted) {
         isSorted = true;
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[i-1] > arr[i]){
+        // sortedNum: use to optimize the number of loop time by cutting the last sorted elements
+        // After one loop the biggest element will be in the right place/ last position
+        let sortedNum = 0;
+        for (let i = 1; i < arr.length - sortedNum; i++) {
+            if (arr[i - 1] > arr[i]) {
                 isSorted = false;
-                const tmp = arr[i-1];
-                arr[i-1] = arr[i]
+                const tmp = arr[i - 1];
+                arr[i - 1] = arr[i]
                 arr[i] = tmp;
             }
-            
+            if (i === arr.length) sortedNum++;
         }
-        
     }
     return arr;
 }
