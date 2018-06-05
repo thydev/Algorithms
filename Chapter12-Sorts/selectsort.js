bdd92b73f0214bf3390926d634c688024a8fe1df
Chanthy Ngin <nginchanthy@gmail.com>
2018-06-05 16:45:06 -0700

    SelectionSoort

--- Chapter12-Sorts/selectsort.js
+++ Chapter12-Sorts/selectsort.js
@@ -1,8 +1,8 @@
-function selectsort(arr){
+function selectsort(arr) {
     for (let i = 0; i < arr.length; i++) {
         let min = arr[i];
         let minIndex = i;
-        for (let j = i+1; j < arr.length; j++) {
+        for (let j = i + 1; j < arr.length; j++) {
             if (arr[j] < min) {
                 min = arr[j];
                 minIndex = j;
