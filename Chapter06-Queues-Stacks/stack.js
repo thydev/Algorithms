2f3bd761291f97e2fffcb0bdc7338270c7d7478b
Chanthy Ngin <nginchanthy@gmail.com>
2018-06-06 19:15:43 -0700

    stack

--- Chapter06-Queues-Stacks/stack.js
+++ Chapter06-Queues-Stacks/stack.js
@@ -1,22 +1,49 @@
-
-function Node(val){
+function Node(val) {
     this.value = val;
     this.next = null;
 }
 
-function Stack(){
+function Stack() {
     this.top = null;
 }
 
-Stack.prototype.push = function(val){
+Stack.prototype.push = function (val) {
     var newnode = new Node(val);
     newnode.next = this.top;
     this.top = newnode;
 }
 
-Stack.prototype.pop = function() {
-    if(this.top == null) return null;
+Stack.prototype.pop = function () {
+    if (this.top == null) return null;
     var val = this.top.val;
     this.top = this.top.next
-    return val;
+    return this.top;
 }
+
+Stack.prototype.display = function () {
+    var runner = this.top;
+    var arrtmp = [];
+    // console.log(runner)
+    while (runner) {
+        arrtmp.push(runner.value);
+        console.log(runner.value)
+        runner = this.pop();
+    }
+    while (arrtmp.length > 0) {
+        this.push(arrtmp.shift())
+    }
+}
+
+var stack = new Stack();
+
+stack.push("Bulbasaur");
+stack.push("Charmander");
+stack.push("Squirtle");
+stack.display();
+
+console.log("====>Taking out Squirtle");
+stack.pop();
+stack.display();
+console.log("====>Adding Pikachu");
+stack.push("Pikachu");
+stack.display();
