5e8cdd6b5216f26ef6c4cb128007d1c6bc56b062
Chanthy Ngin <nginchanthy@gmail.com>
2018-06-10 19:20:10 -0700

    isUnique fast false

--- CTCI/isUnique.js
+++ CTCI/isUnique.js
@@ -16,4 +16,21 @@ function isUnique(str) {
 
 console.log(isUnique('asdf')) // true
 console.log(isUnique('asdaf')) // false
-console.log(isUnique('cat or pig')) // true
+console.log(isUnique('cat or pig')) // false
+console.log(isUnique('cat orpig')) // true
+
+function isUnique2(str) {
+    let charDict = {}
+    for (let i = 0; i < str.length; i++) {
+        if (charDict.hasOwnProperty(str[i])) {
+            return false;
+        } else {
+            charDict[str[i]] = str[i];
+        }
+    }
+    return true;
+}
+console.log('========Number 2 solution=========')
+console.log(isUnique2('asdf')) // true
+console.log(isUnique2('asdaf')) // false
+console.log(isUnique2('cat or pig')) // false
