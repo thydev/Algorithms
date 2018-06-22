8ccbf0fd60de359cc59f2c604f6b53ae961c8045
thydev <nginchanthy@gmail.com>
2018-06-21 22:59:58 -0700

    the seconde largest value

--- secondLargest.js
+++ secondLargest.js
@@ -1,1 +1,20 @@
-function secondLargest(arr) {}
+function secondLargest(arr) {
+  if (arr.length < 2) return arr[0];
+  let max1 = arr[0];
+  let max2 = arr[1];
+  if (max2 > max1) {
+    max2 = max1;
+    max1 = arr[1];
+  }
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i] > max1) {
+      max2 = max1;
+      max1 = arr[i];
+    }
+  }
+  return max2;
+}
+
+console.log(secondLargest([2, 3, 6, 12]));
+console.log(secondLargest([22, 21, 16, 12]));
+console.log(secondLargest([9, 13, 6, 12, 12]));
