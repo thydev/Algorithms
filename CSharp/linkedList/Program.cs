132c7c123d4e02481a81d1e63c3ebb5b2f13ddd9
thydev <nginchanthy@gmail.com>
2018-06-29 21:15:05 -0700

    Singly Linked List

---
+++ CSharp/linkedList/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace linkedList
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      SinglyLinkedList sl1 = new SinglyLinkedList();
+      sl1.insertFirst(12);
+      sl1.insertFirst(23);
+      sl1.insertFirst(32);
+      sl1.insertFirst(90);
+      sl1.insertLast(999);
+      sl1.displayList();
+      sl1.deleteFirst();
+      sl1.displayList();
+    }
+  }
+
+  public class SinglyLinkedList
+  {
+    private Node head;
+    public bool isEmpty()
+    {
+      return (head == null);
+    }
+
+    public void insertFirst(int data)
+    {
+      Node newNode = new Node();
+      newNode.data = data;
+      newNode.next = head;
+      head = newNode;
+    }
+
+    public Node deleteFirst()
+    {
+      Node temp = head;
+      head = head.next;
+      return temp;
+    }
+
+    public void displayList()
+    {
+      System.Console.WriteLine("Displaying.....");
+      Node current = head;
+      while (current != null)
+      {
+        current.displayNoe();
+        current = current.next;
+      }
+    }
+
+    public void insertLast(int data)
+    {
+      Node current = head;
+      while (current.next != null)
+      {
+        current = current.next;
+      }
+      Node newNode = new Node();
+      newNode.data = data;
+      current.next = newNode;
+    }
+  }
+
+  public class Node
+  {
+    public int data;
+    public Node next;
+
+    public void displayNoe()
+    {
+      Console.WriteLine("<" + data + ">");
+    }
+  }
+
+}
--- Chapter11-Trees/bst.js
+++ Chapter11-Trees/bst.js
@@ -1,46 +1,82 @@
 // Binary Search Tree
 
 class Node {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 class BST {
-    constructor() {
-        this.root = null;
+  constructor() {
+    this.root = null;
+  }
+
+  add(val, node) {
+    if (this.root === null) {
+      this.root = new Node(val);
+      return this;
     }
-}
-
-BST.prototype.Add = (val, node = this.root) => {
-
     if (!node) {
-        node = new Node(val);
-        return this;
+      node = new Node(val);
+      // this.root = node;
+      return this;
     }
 
     if (val < node.val) {
-        this.Add(val, node.left);
+      this.add(val, node.left);
     } else {
-        this.Add(val, node.right);
+      this.add(val, node.right);
     }
+  }
 
+  printAll(node = this.root) {
+    console.log(node, 'in print');
+    if (node) {
+      console.log(node.val);
+      this.printAll(node.left);
+      this.printAll(node.right);
+    }
+  }
+}
+
+// BST.prototype.add = function(val, node = this.root) {
+//   console.log(node);
+//   if (!node) {
+//     node = new Node(val);
+//     // this.root = node;
+//     console.log(node);
+//     return this;
+//   }
+
+//   if (val < node.val) {
+//     this.add(val, node.left);
+//   } else {
+//     this.add(val, node.right);
+//   }
+// };
+
+BST.prototype.size = function(node = this.root, count = 0) {
+  if (!node) return count;
+  count++;
+  count = this.size(node.left, count);
+  count = this.size(node.right, count);
+  return count;
 };
 
-BST.prototype.Size = (node = this.root, count = 0) => {
-    if (!node) return count;
-    count++;
-    count = this.Size(node.left, count);
-    count = this.Size(node.right, count);
-    return count;
-}
+// BST.prototype.printAll = function(node = this.root) {
+//   console.log(node, 'in print');
+//   if (node) {
+//     console.log(node.val);
+//     this.printAll(node.left);
+//     this.printAll(node.right);
+//   }
+// };
 
-BST.prototype.PrintAll = (node = this.root) => {
-    if (node) {
-        console.log(node.val);
-        this.PrintAll(node.left);
-        this.PrintAll(node.right);
-    }
-}
+let bst1 = new BST();
+bst1.add(6);
+bst1.add(7);
+bst1.add(10);
+bst1.add(20);
+bst1.printAll();
