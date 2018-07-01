364356b4b0ee83093f2e9d7003a9da5d065e054a
thydev <nginchanthy@gmail.com>
2018-06-30 22:25:19 -0700

    bst

--- Chapter11-Trees/bst.js
+++ Chapter11-Trees/bst.js
@@ -6,6 +6,22 @@ class Node {
     this.left = null;
     this.right = null;
   }
+
+  add(node) {
+    if (node.val > this.val) {
+      if (this.right) {
+        this.right.add(node);
+      } else {
+        this.right = node;
+      }
+    } else {
+      if (this.left) {
+        this.left.add(node);
+      } else {
+        this.left = node;
+      }
+    }
+  }
 }
 
 class BST {
@@ -14,27 +30,30 @@ class BST {
   }
 
   add(val, node) {
+    const newNode = new Node(val);
     if (this.root === null) {
-      this.root = new Node(val);
-      return this;
-    }
-    if (!node) {
-      node = new Node(val);
-      // this.root = node;
+      this.root = newNode;
       return this;
     }
 
-    if (val < node.val) {
-      this.add(val, node.left);
-    } else {
-      this.add(val, node.right);
-    }
+    this.root.add(newNode);
+    return this;
+    // if (!node) {
+    //   node = new Node(val);
+    //   // this.root = node;
+    //   return this;
+    // }
+
+    // if (val < node.val) {
+    //   this.add(val, node.left);
+    // } else {
+    //   this.add(val, node.right);
+    // }
   }
 
   printAll(node = this.root) {
-    console.log(node, 'in print');
+    if (node) console.log(node.val, 'in print');
     if (node) {
-      console.log(node.val);
       this.printAll(node.left);
       this.printAll(node.right);
     }
@@ -75,8 +94,8 @@ BST.prototype.size = function(node = this.root, count = 0) {
 // };
 
 let bst1 = new BST();
+bst1.add(10);
 bst1.add(6);
 bst1.add(7);
-bst1.add(10);
 bst1.add(20);
-bst1.printAll();
+bst1.printAll(bst1.root);
