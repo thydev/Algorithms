0122ff535b91ddcf08eeefdb7352799cc10aa6f8
Chanthy Ngin <nginchanthy@gmail.com>
2018-07-10 19:34:19 -0700

    valid parentheses

--- CSharp/stack/Program.cs
+++ CSharp/stack/Program.cs
@@ -6,45 +6,63 @@ namespace stack
     {
         static void Main(string[] args)
         {
+            string s1 = "string1";
+            string temp = s1;
+            // temp = "temp string";
+            s1 = "string2";
+            System.Console.WriteLine(s1);
+            System.Console.WriteLine(temp);
             Console.WriteLine("Hello World!");
         }
     }
 
-    public class Stack {
+    public class Stack
+    {
         private int maxSize;
         private string[] stackArray;
         private int top;
 
-        public Stack(int size) {
+        public Stack(int size)
+        {
             maxSize = size;
             stackArray = new string[maxSize];
             top = -1;
         }
 
-        public void push(string m) {
-            if (isFull()) {
+        public void push(string m)
+        {
+            if (isFull())
+            {
                 System.Console.WriteLine("This stack is full");
-            } else {
+            }
+            else
+            {
                 top++;
                 stackArray[top] = m;
             }
         }
 
-        public string pop() {
-            if (isEmpty()) {
+        public string pop()
+        {
+            if (isEmpty())
+            {
                 System.Console.WriteLine("The stack is empty.");
                 return "--";
-            } else {
+            }
+            else
+            {
                 int old_top = top;
                 top--;
                 return stackArray[old_top];
             }
         }
-        private bool isFull() {
+        private bool isFull()
+        {
             return (maxSize - 1 == top);
         }
 
-        public bool isEmpty() {
+        public bool isEmpty()
+        {
             return top == -1;
         }
     }
--- validBraces.js
+++ validBraces.js
@@ -1,12 +1,35 @@
-
-function validBraces(str){
+function validBraces(str) {
     var brace = {
-        "(" : ")",
-        "[" : "]",
-        "{" : "}"
+        "(": ")",
+        "[": "]",
+        "{": "}"
     }
     var orders = []
-    for(var i = 0; i < str.length; i++){
-        
+    for (var i = 0; i < str.length; i++) {
+        if (orders.length === 0) {
+            // if this is the right part, return false
+            if (!brace[str[i]]) {
+                return false;
+            }
+
+            // if this is the left part, add it to the array
+            orders.push(str[i]);
+        } else {
+            // if there is a left and the next char is the right, then remove it from the array orders
+            if (brace[orders[orders.length - 1]] === str[i]) {
+                orders.splice(orders.length - 1, 1);
+            } else {
+                // this is the left brace
+                orders.push(str[i]);
+            }
+        }
     }
+    // if there is nothing inside the array, this is valid
+    return orders.length === 0;
 }
+
+console.log(validBraces('()()[[[]]]'));
+console.log(validBraces('()()[[[]]]]]'));
+console.log(validBraces(']'));
+console.log(validBraces('['));
+console.log(validBraces('[{()}]{}'));
